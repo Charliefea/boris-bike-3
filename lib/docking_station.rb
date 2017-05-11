a04a9b61be8a8209c2def0558076af1f9bd51a6d
charliefea <charlotte.feather@yahoo.com>
2017-05-11 13:51:10 +0100

    Completed Challenge 17

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,21 +1,38 @@
 require './lib/bike.rb'
 
 class DockingStation
-  attr_reader :bike
+
+  attr_reader :bikes, :capacity
+
+  DEFAULT_CAPACITY = 20
+  def initialize (capacity = DEFAULT_CAPACITY)
+      @bikes =[]
+      @capacity = capacity
+  end
 
   def release_bike
-    if bike == nil
+    if empty?
       raise('Sorry there are no bikes')
     else
-      bike
+      bikes.pop
     end
   end
 
   def docks_bike (bikeinstance)
-    if bike != nil
+    if full?
       raise('There are no spaces left')
     else
-      @bike = bikeinstance
+      bikes.push(bikeinstance)
+      bikes[-1]
     end
+  end
+
+private
+  def full?
+    return true if bikes.length == DEFAULT_CAPACITY
+  end
+
+  def empty?
+    return true if bikes.empty?
   end
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -12,16 +12,16 @@ describe DockingStation do
 
   it { is_expected.to respond_to :docks_bike }
 
-  it "docks bike" do
+  it "docks bike and gives value of bike" do
     bike = Bike.new
     docked_bike = subject.docks_bike(bike)
     expect(docked_bike).to eq bike
   end
 
-  it "returns docked bike" do
+  it "returns docked bike and gives value of last docked bike" do
     bike = Bike.new
     subject.docks_bike(bike)
-    expect(subject.bike).to eq bike
+    expect(subject.bikes[-1]).to eq bike
   end
 
   it "does not release a bike if there are no bikes available" do
@@ -34,10 +34,22 @@ describe DockingStation do
     expect(subject.release_bike).to eq bike
   end
 
-  it "does not release a bike if there are no bikes docked" do
-    bike = Bike.new
-    subject.docks_bike(bike)
-    bike2 = Bike.new
-    expect { subject.docks_bike(bike2)}.to raise_error('There are no spaces left')
+  it "doesn't allow more bikes than spaces to dock" do
+    subject.capacity.times {subject.docks_bike Bike.new}
+    bikeextra = Bike.new
+    expect { subject.docks_bike(bikeextra)}.to raise_error('There are no spaces left')
   end
+
+  it { is_expected.to respond_to :capacity }
+
+  it 'the capacity of the station can be set by the user' do
+    station = DockingStation.new 30
+    expect(station.capacity).to eq 30
+  end
+
+  it 'the capacity of the station defaults to 20' do
+    station = DockingStation.new
+    expect(station.capacity).to eq DockingStation::DEFAULT_CAPACITY
+  end
+
 end
