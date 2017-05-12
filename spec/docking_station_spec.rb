46d5aff1e84083cde7254c8614aae2f1c70527c7
charliefea <charlotte.feather@yahoo.com>
2017-05-12 11:43:49 +0100

    Completed Challenge 20

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -3,42 +3,35 @@ require 'docking_station'
 describe DockingStation do
   it { is_expected.to respond_to :release_bike }
 
+  let (:bike) { double( :bike ) }
+
   it "releases working bikes" do
-    bike = Bike.new
-    subject.docks_bike(bike)
-    bike = subject.release_bike
-    expect(bike).to be_working
+    workingbike = double("workingbike", :working? => true)
+    subject.docks_bike(workingbike)
+    workingbike = subject.release_bike
+    expect(workingbike).to be_working
   end
 
   it { is_expected.to respond_to :docks_bike }
 
   it "docks bike and gives value of bike" do
-    bike = Bike.new
     docked_bike = subject.docks_bike(bike)
     expect(docked_bike).to eq bike
   end
-
-  #  TEST no longer valid as bikes is now a private method
-  #it "returns docked bike" do
-  #   bike = Bike.new
-  #   subject.docks_bike(bike)
-  #   expect(subject.bikes).to eq bike
-  # end
 
   it "does not release a bike if there are no bikes available" do
     expect { subject.release_bike }.to raise_error("Sorry there are no bikes")
   end
 
   it "releases same bike as docked" do
-    bike = Bike.new
-    subject.docks_bike(bike)
-    expect(subject.release_bike).to eq bike
+    workingbike = double("workingbike", :working? => true)
+    subject.docks_bike(workingbike)
+    expect(subject.release_bike).to eq workingbike
   end
 
   it "doesn't allow more bikes than spaces to dock" do
-    subject.capacity.times {subject.docks_bike Bike.new}
-    bikeextra = Bike.new
-    expect { subject.docks_bike(bikeextra)}.to raise_error('There are no spaces left')
+    subject.capacity.times {subject.docks_bike bike}
+    expect { subject.docks_bike(bike)}.to raise_error('There are no spaces left')
   end
 
   it { is_expected.to respond_to :capacity }
@@ -50,23 +43,19 @@ describe DockingStation do
 
   it 'the capacity of the station defaults to 20' do
     station = DockingStation.new
-    expect(station.capacity).to eq DockingStation::DEFAULT_CAPACITY
+    expect(subject.capacity).to eq DockingStation::DEFAULT_CAPACITY
   end
 
   it 'the docking station does not release a broken bike' do
-    bike1 = Bike.new
-    subject.docks_bike(bike1.broken)
+    brokenbike = double("brokenbike", :working? => false)
+    subject.docks_bike(brokenbike)
     expect { subject.release_bike}.to raise_error('This bike does not work')
   end
 
   it 'the docking station will accept broken and working bikes' do
-    bikeworking = Bike.new
-    expect(subject.docks_bike(bikeworking)).to eq(bikeworking)
-    biketobebroken = Bike.new
-    expect(subject.docks_bike(biketobebroken.broken)).to eq(biketobebroken)
+    workingbike = double("workingbike", :working? => true)
+    brokenbike = double("brokenbike", :working? => false)
+    expect(subject.docks_bike(workingbike)).to eq(workingbike)
+    expect(subject.docks_bike(brokenbike)).to eq(brokenbike)
   end
-
-
-
-
 end
